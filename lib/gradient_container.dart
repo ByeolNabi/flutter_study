0fe84487f8d3885179a66bdbe376067f54834521
ByeolNabi <20183338@office.deu.ac.kr>
2024-06-25 14:15:36 +0900

    align center

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,14 +1,24 @@
 import 'package:flutter/material.dart';
+
 import 'package:flutter_study/styledText.dart';
 
 var startAlignment = Alignment.topLeft;
 var endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer(this.color1, this.color2,{super.key});
+  const GradientContainer(this.color1, this.color2, {super.key});
+
+  // 생성자 함수? 이렇게 생성자 함수를 만들면 gradient color를 바로바로 만들 수 있음.
+  const GradientContainer.purple({super.key})
+      : color1 = Colors.deepPurple,
+        color2 = Colors.indigo;
 
   final Color color1;
   final Color color2;
+
+  void rollDice() {
+    // ...
+  }
 
   @override
   Widget build(context) {
@@ -20,7 +30,21 @@ class GradientContainer extends StatelessWidget {
           end: endAlignment,
         ),
       ),
-      child: const Center(child: StyledText('Hello World!')),
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min, // Column의 요소를 중간으로 정렬
+          children: [
+            Image.asset(
+              'assets/images/dice-2.png',
+              width: 200,
+            ),
+            TextButton(
+              onPressed: rollDice,
+              child: const Text('Roll Dice'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
