cc2923573a6c0f0a4cfd852b958f43db75702736
ByeolNabi <20183338@office.deu.ac.kr>
2024-07-03 15:11:21 +0900

    section2 init

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
 
-import 'package:flutter_study/gradient_container.dart';
-
 void main() {
   runApp(
     const MaterialApp(
       home: Scaffold(
-        body: GradientContainer(
-          Color.fromARGB(255, 127, 53, 255),
-          Color.fromARGB(255, 187, 154, 245),
-        ),
+        body: Text("hello world"),
       ),
     ),
   );
 }
+
+
+/*
+중앙에 정렬시키고 싶다.
+  center?
+세로로 정렬하고 싶다.
+  column
+ */
