47171a704f4b47fdf6c1ea9cb2dd8b5be4b44713
ByeolNabi <20183338@office.deu.ac.kr>
2024-07-05 15:30:25 +0900

    [s2] add image

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,17 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        body: Text("hello world"),
+        backgroundColor: Colors.deepPurple,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset('assets/images/quiz-logo.png', width: 200),
+            ],
+          ),
+        ),
       ),
     ),
   );
@@ -15,5 +23,5 @@ void main() {
 중앙에 정렬시키고 싶다.
   center?
 세로로 정렬하고 싶다.
-  column
+  column?
  */
