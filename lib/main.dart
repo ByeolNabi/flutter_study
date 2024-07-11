f0280b83ce2d577542fd198c3f54ce937e9169e2
ByeolNabi <20183338@office.deu.ac.kr>
2024-07-11 18:52:11 +0900

    add gradient start point

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,8 @@ void main() {
                 Color.fromARGB(255, 78, 13, 151),
                 Color.fromARGB(255, 107, 15, 168),
               ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
             ),
           ),
           child: const StartScreen(),
@@ -25,28 +27,28 @@ void main() {
 
 /*
 Scaffold(
-        backgroundColor: Colors.deepPurple,
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset('assets/images/quiz-logo.png', width: 200),
-              const SizedBox(height: 20),
-              const Text(
-                "Learn Flutter the fun way!",
-                style: TextStyle(
-                  color: Colors.white,
-                ),
-              ),
-              const SizedBox(height: 10),
-              ElevatedButton(
-                child: const Text('Fly me to the moon'),
-                onPressed: () {
-                  // ...
-                },
-              ),
-            ],
+  backgroundColor: Colors.deepPurple,
+  body: Center(
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Image.asset('assets/images/quiz-logo.png', width: 200),
+        const SizedBox(height: 20),
+        const Text(
+          "Learn Flutter the fun way!",
+          style: TextStyle(
+            color: Colors.white,
           ),
         ),
-      ),
+        const SizedBox(height: 10),
+        ElevatedButton(
+          child: const Text('Fly me to the moon'),
+          onPressed: () {
+            // ...
+          },
+        ),
+      ],
+    ),
+  ),
+),
  */
