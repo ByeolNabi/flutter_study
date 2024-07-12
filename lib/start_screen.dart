37db94febb27251aa689e2d8e9d0e450fd7699c1
ByeolNabi <20183338@office.deu.ac.kr>
2024-07-12 18:52:48 +0900

    add images and text

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -5,6 +5,24 @@ class StartScreen extends StatelessWidget {
 
   @override
   Widget build(context) {
-    return const Center(child: Text("hello world"));
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300,
+          ),
+          const SizedBox(height: 20),
+          const Text(
+            "learn flutter the fun way!",
+            style: TextStyle(
+              color: Color.fromARGB(255, 245, 239, 255),
+              fontSize: 30,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
