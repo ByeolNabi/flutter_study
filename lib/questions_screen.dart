04a3f030b360006ed500d75f5f2a052f5a947f3e
ByeolNabi <20183338@office.deu.ac.kr>
2024-07-16 15:47:25 +0900

    make file questions_screen.dart

---
+++ lib/questions_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class QuestionsScreen extends StatefulWidget {
+  const QuestionsScreen({super.key});
+
+  @override
+  State<QuestionsScreen> createState() => _QuestionsScreen();
+}
+
+class _QuestionsScreen extends State<QuestionsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(color: const Color(0xFFFFE306));
+  }
+}
